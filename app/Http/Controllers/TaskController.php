c668bfda7a53a6256e86c0da74a46bd2ad3a66a3
Yana <masatomo.yanagihara.jp@gmail.com>
2020-09-09 16:52:47 +0900

    タスク登録API実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,12 +3,18 @@
  namespace App\Http\Controllers;
 
  use App\Task;
+ use Illuminate\Http\Request;
 
  class TaskController extends Controller
  {
      public function index()
      {
          return Task::all(); // Taskモデルから全件取得してリターン
+     }
+
+     public function store(Request $request)
+     {
+         return Task::create($request->all()); // リクエストで受け取ったデータを登録
      }
 
      public function show(Task $task)
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/tasks', 'TaskController@index'); // /api/tasks/
+Route::post('/tasks', 'TaskController@store');
+
 Route::get('/tasks/{task}', 'TaskController@show');
